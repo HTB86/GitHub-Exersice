98141345ea11969875cc1fb7a0d65486bf59b156
Hassan Boaliyan <hassan.boaliyan@hotmail.com>
2024-05-14 20:27:38 +0300

    Initial Commit

---
+++ GitHub Exersice/GitHub_ExersiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExersiceApp.swift
+//  GitHub Exersice
+//
+//  Created by Hassan Boaliyan on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExersiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
