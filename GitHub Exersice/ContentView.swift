f67f29a6195ec85202b2b36733ab38d88d7bc0a2
Hassan Boaliyan <hassan.boaliyan@hotmail.com>
2024-05-14 20:27:38 +0300

    Initial Commit

---
+++ GitHub Exersice/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  GitHub Exersice
+//
+//  Created by Hassan Boaliyan on 14/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+                
+            
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
+        }
+        .padding()
+        .foregroundColor(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
